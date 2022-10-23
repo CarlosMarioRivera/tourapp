5e6eec935eefd7870728dc0591a40d00d1e4b4f5
CarlosMarioRivera <111686447+CarlosMarioRivera@users.noreply.github.com>
2022-10-23 13:47:35 -0500

    Subir Registraser

--- lib/pages/registrar_page_tourapp.dart
+++ lib/pages/registrar_page_tourapp.dart
@@ -13,32 +13,163 @@ class _RegistrarPageState extends State<RegistrarPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.yellow,
-        body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    child: const Image(image: AssetImage("/icono.png"),width: 50, height: 50),
-                       padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                       margin: const EdgeInsets.all(20),
-                       decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(200),
-                          border: Border.all(color: Colors.black38, width: 4),
-                          color: Colors.blue,
-                       ),
+        body: Column(
+          children: [
+             Container(
+                child: const Image(image: AssetImage("/icono.png"),width: 50, height: 50),
+                padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                margin: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                   borderRadius: BorderRadius.circular(200),
+                   border: Border.all(color: Colors.black38, width: 4),
+                   color: Colors.blue,
                 ),
-                SizedBox(height: 20,),
-                TextButton(
-                    style: TextButton.styleFrom(
-                        textStyle: const TextStyle(fontSize: 16, fontStyle: FontStyle.italic, color: Colors.lightBlueAccent)
-                    ),
-                    onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage()));
-                    },
-                    child: const Text("Devolverse")
-                ),
-              ],
+             ),
+            Text('Información del Registro', style: Theme.of(context).textTheme.headline5,
             ),
+             Container(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                   keyboardType: TextInputType.text,
+                   textCapitalization: TextCapitalization.words,
+                   textAlign: TextAlign.center,
+                   style: TextStyle(
+                      color: Colors.red,
+                   ),
+                   decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.notifications_active, color: Colors.lightBlue,),
+                      hintText: "Nombres",
+                      hintStyle: TextStyle(color: Colors.black),
+                      helperText: "Digita tus Nombres Completos",
+                      contentPadding: EdgeInsets.all(10),
+                      enabledBorder: OutlineInputBorder(
+                         borderRadius: BorderRadius.all(Radius.circular(20)),
+                         borderSide: BorderSide(
+                            color: Colors.lightBlue,
+                            width: 3
+                         )
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
+                         borderSide: BorderSide(
+                             color: Colors.black,
+                             width: 3
+                         )
+                     )
+                  ),
+                ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                keyboardType: TextInputType.text,
+                textCapitalization: TextCapitalization.words,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.notifications_active, color: Colors.lightBlue,),
+                    hintText: "Apellidos",
+                    hintStyle: TextStyle(color: Colors.black),
+                    helperText: "Digita tus Apellidos Completos",
+                    contentPadding: EdgeInsets.all(10),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        borderSide: BorderSide(
+                            color: Colors.lightBlue,
+                            width: 3
+                        )
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 3
+                        )
+                    )
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                textCapitalization: TextCapitalization.words,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.notifications_active, color: Colors.lightBlue,),
+                    hintText: "E-Mail",
+                    hintStyle: TextStyle(color: Colors.black),
+                    helperText: "Digita tu Correo Electrónico",
+                    contentPadding: EdgeInsets.all(10),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        borderSide: BorderSide(
+                            color: Colors.lightBlue,
+                            width: 3
+                        )
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 3
+                        )
+                    )
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: TextField(
+                obscureText: true,
+                keyboardType: TextInputType.text,
+                textCapitalization: TextCapitalization.words,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.red,
+                ),
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.notifications_active, color: Colors.lightBlue,),
+                    hintText: "Contraseña",
+                    hintStyle: TextStyle(color: Colors.black),
+                    helperText: "Digita tu Contraseña",
+                    contentPadding: EdgeInsets.all(10),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        borderSide: BorderSide(
+                            color: Colors.lightBlue,
+                            width: 3
+                        )
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(
+                            color: Colors.black,
+                            width: 3
+                        )
+                    )
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed:(){},
+              child: const Text("Guardar Cambios"),
+            ),
+            Container(
+               padding: EdgeInsets.all(10),
+               child: TextButton(
+                  style: TextButton.styleFrom(
+                      textStyle: const TextStyle(fontSize: 16, fontStyle: FontStyle.italic, color: Colors.lightBlueAccent)),
+                  onPressed: (){Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage()));},
+                  child: const Text("Regresar")
+               ),
+            ),
+          ]
         ),
     );
   }
